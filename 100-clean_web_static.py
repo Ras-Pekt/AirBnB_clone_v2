1365a9f3b2f6d905e621b27079e873dda43c6a35
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-10-08 21:35:04 +0300

    fix:clean web static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -85,17 +85,18 @@ def do_clean(number=0):
     """
     deletes out-of-date archives
     """
-    if number < 0:
-        return
-    else:
-        try:
-            number = int(number)
-            if number == 0 or number == 1:
-                number = 2
-            else:
-                number += 1
-        except Exception as e:
+    try:
+        number = int(number)
+
+        if number < 0:
             return
+
+        if number == 0 or number == 1:
+            number = 2
+        else:
+            number += 1
+    except Exception as e:
+        return
 
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
